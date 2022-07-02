c2795c675871613ead6cd8af6d072e9993fe44e8
Geon Hyuk Im <31177070+Geon-Hyuk@users.noreply.github.com>
2022-07-02 14:00:03 +0900

    Time: 18 ms (82.54%), Space: 59 MB (76.37%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
@@ -1,57 +1,29 @@
 class Solution {
-    int h, w;
     public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
-        
-        this.h = h;
-        this.w = w;
-        int m = 1000000007;
+
+        // Start by sorting the inputs
         Arrays.sort(horizontalCuts);
         Arrays.sort(verticalCuts);
+        int n = horizontalCuts.length;
+        int m = verticalCuts.length;
         
-        
-        long maxHeight = calculateHeight(horizontalCuts);
-        long maxWidth = calculateWidth(verticalCuts);
-        long maxArea = maxHeight * maxWidth;
-        System.out.println(maxArea);      
-        return (int) (maxArea % m);
-    }
-    
-    public long calculateHeight(int[] horizontalCuts) {
-        int startRow = 0;
-        int endRow = h;
-        int maximumHeight = Integer.MIN_VALUE;
-        
-        for (int i = 0; i < horizontalCuts.length; i++) {
-            int currentRow = horizontalCuts[i];
-            int height;
-            
-            if (i == horizontalCuts.length - 1) {
-                height = Math.max(currentRow - startRow, endRow - currentRow);                
-            } else {
-                height = currentRow - startRow;
-            }
-            maximumHeight = Math.max(maximumHeight, height);
-            startRow = horizontalCuts[i];           
+        // Consider the edges first
+        long maxHeight = Math.max(horizontalCuts[0], h - horizontalCuts[n - 1]);
+        for (int i = 1; i < n; i++) {
+            // horizontalCuts[i] - horizontalCuts[i - 1] represents the distance between
+            // two adjacent edges, and thus a possible height
+            maxHeight = Math.max(maxHeight, horizontalCuts[i] - horizontalCuts[i - 1]);
         }
-        return maximumHeight;  
-    }
-    
-    public long calculateWidth(int[] verticalCuts) {
-        int startColumn = 0;
-        int endColumn = w;
-        int maximumWidth = Integer.MIN_VALUE;
         
-        for (int i = 0; i < verticalCuts.length; i++) {
-            int currentColumn = verticalCuts[i];
-            int width;
-            if (i == verticalCuts.length - 1) {
-                width = Math.max(currentColumn - startColumn, endColumn - currentColumn);
-            } else {
-                width = currentColumn - startColumn;                
-            }
-            maximumWidth = Math.max(maximumWidth, width);
-            startColumn = verticalCuts[i];
+        // Consider the edges first
+        long maxWidth = Math.max(verticalCuts[0], w - verticalCuts[m - 1]);
+        for (int i = 1; i < m; i++){
+            // verticalCuts[i] - verticalCuts[i - 1] represents the distance between
+            // two adjacent edges, and thus a possible width
+            maxWidth = Math.max(maxWidth, verticalCuts[i] - verticalCuts[i - 1]);
         }
-        return maximumWidth;
+
+        // Be careful of overflow, and don't forget the modulo!
+        return (int) ((maxWidth * maxHeight) % (1000000007));
     }
 }
