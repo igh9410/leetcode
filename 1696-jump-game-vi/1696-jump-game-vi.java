82d74b195defec8ed2d03efc619829af3bdbee70
Geon Hyuk Im <31177070+Geon-Hyuk@users.noreply.github.com>
2022-07-09 11:08:26 +0900

    Time: 95 ms (28.50%), Space: 115 MB (27.32%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.java
+++ 1696-jump-game-vi/1696-jump-game-vi.java
@@ -3,19 +3,15 @@ class Solution {
         int n = nums.length;
         int[] dp = new int[n];
         dp[0] = nums[0];
-        Deque<Integer> dq = new LinkedList<>();
-        dq.offerLast(0);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[0] - a[0]);
+        pq.add(new int[] {nums[0], 0});
         for (int i = 1; i < n; i++) {
             
-            while (dq.peekFirst() != null && dq.peekFirst() < i - k) {
-                dq.pollFirst();
+            while (pq.peek()[1] < i - k) {
+                pq.remove();
             }
-            dp[i] = dp[dq.peek()] + nums[i];
-            
-            while (dq.peekLast() != null && dp[i] >= dp[dq.peekLast()]) {
-                dq.pollLast();
-            }
-            dq.offerLast(i);
+            dp[i] = nums[i] + dp[pq.peek()[1]];
+            pq.add(new int[]{ dp[i], i });                
         }
         return dp[n - 1];
     }
