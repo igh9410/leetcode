cd511600244ccb465a37e794a068df3c6bdbb9c6
Geon Hyuk Im <athanasia9410@gmail.com>
2022-07-16 23:19:26 +0900

    Time: 5 ms (92.71%), Space: 43.2 MB (30.42%) - LeetHub

---
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.java
@@ -0,0 +1,26 @@
+class Solution {
+    final int div = 1000000007;
+    public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        int[][][] memo = new int[m][n][maxMove + 1];
+        for (int[][] l : memo) {
+            for (int[] s1: l) {
+                Arrays.fill(s1, -1);
+            }
+        }
+        return findPaths(m, n, maxMove, startRow, startColumn, memo);
+    }
+    
+    public int findPaths(int m, int n, int N, int i, int j, int[][][] memo) {
+        if (i == m || j == n || i < 0 || j < 0)
+            return 1;
+        if (N == 0) 
+            return 0;
+        if (memo[i][j][N] >= 0)
+            return memo[i][j][N];
+        memo[i][j][N] = (
+            (findPaths(m, n, N - 1, i - 1, j, memo) + findPaths(m, n, N - 1, i + 1, j, memo)) % div +
+            (findPaths(m, n, N - 1, i, j -1, memo) + findPaths(m, n, N - 1, i, j + 1, memo)) % div
+        ) % div;
+        return memo[i][j][N];
+    }
+}
