eba3c4f5ef9aebd0938bf6dbfdf8043bb73a9b58
Geon Hyuk Im <athanasia9410@gmail.com>
2022-07-28 20:34:14 +0900

    Time: 33 ms (10.98%), Space: 46.2 MB (23.74%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        
+        Map<Character, Integer> sMap = new HashMap<>();
+        Map<Character, Integer> tMap = new HashMap<>();
+        
+        int n = s.length();
+        
+        for (int i = 0; i < n; i++) {
+            sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0) + 1);
+            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0) + 1);
+        }
+        
+        return (sMap.equals(tMap));
+    }
+}
