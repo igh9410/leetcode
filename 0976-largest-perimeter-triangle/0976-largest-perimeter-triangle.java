17609bbc89730d30c6a881910bfc57bbd835230c
Geon Hyuk Im <athanasia9410@gmail.com>
2022-10-12 21:19:35 +0900

    Time: 11 ms (72.56%), Space: 53.1 MB (85.49%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.java
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.java
@@ -1,22 +1,9 @@
 class Solution {
-    public int largestPerimeter(int[] nums) {
-        Arrays.sort(nums);
-        int longestLength = nums[nums.length - 1];
-        int result = 0;
-        
-        for (int i = nums.length - 1; i >= 2; i--) {
-            int first = nums[i - 2];
-            int second = nums[i - 1];
-            if (longestLength >= first + second) {
-                longestLength = second;
-                continue;
-            }
-            else {
-                result = first + second + longestLength;
-                break;
-            }    
-        }
-        
-        return result;
+    public int largestPerimeter(int[] A) {
+        Arrays.sort(A);
+        for (int i = A.length - 3; i >= 0; --i)
+            if (A[i] + A[i+1] > A[i+2])
+                return A[i] + A[i+1] + A[i+2];
+        return 0;
     }
 }
