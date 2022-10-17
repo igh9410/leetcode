0c80b3295c082917b949eda534ae94f94248ccbc
Geon Hyuk Im <athanasia9410@gmail.com>
2022-10-17 15:08:55 +0900

    Time: 6 ms (29.40%), Space: 42.6 MB (9.15%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -4,6 +4,9 @@ class Solution {
         
         for (int i = 0; i < sentence.length(); i++) {
             scanMap.put(sentence.charAt(i), scanMap.getOrDefault(sentence.charAt(i), 0) + 1);
+            if (scanMap.size() == 26) {
+                return true;
+            }
         }
         return (scanMap.size() == 26);
     }
