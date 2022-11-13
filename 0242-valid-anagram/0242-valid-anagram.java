0bf5ba89ebfbc993a19408eedbd10e6705ea04c1
Geon Hyuk Im <athanasia9410@gmail.com>
2022-11-13 17:59:49 +0900

    Time: 3 ms (95.97%), Space: 42.4 MB (92.85%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,21 +1,9 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length())
-            return false;
-        
-        int[] freqS = new int[26];
-        int[] freqT = new int[26];
-        
-        for (int i = 0; i < s.length(); i++) {
-            freqS[s.charAt(i) - 'a']++;
-            freqT[t.charAt(i) - 'a']++;
-        }
-        
-        for (int i = 0; i < 26; i++) {
-            if (freqS[i] != freqT[i])
-                return false;
-        }
-        
-        return true;
+     char[] str1 = s.toCharArray();
+    char[] str2 = t.toCharArray();
+    Arrays.sort(str1);
+    Arrays.sort(str2);
+    return Arrays.equals(str1, str2);
     }
 }
